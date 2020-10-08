bf2d708128199d69e7ad6f02dba3e2451367f9f7
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 09:40:37 +0700

    Add files via upload

---
+++ Tugas1_1.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>Daftar Menu</h1>";
+
+echo "<li>Ayam Goreng = Rp.13000
+<li>Ayam Bakar = Rp.15000
+<li>Ayam Sayur = Rp.13000
+<li>Tempe Goreng = Rp.1000
+<li>Tahu Goreng = Rp.1000
+<li>Soto = Rp.3000
+<li>Nasi Putih = Rp.5000
+<li>Es Teh = Rp.4000
+<li>Es Tebu = Rp.5000"
+?>
